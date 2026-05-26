159c9b5b1fd254a5b943eb8ad8e4fe9502c1cb44
bakar255 <aboubakari.camarargy@gmail.com>
2026-05-26 11:14:18 +0200

    refactor: add new .env variable, change css selectors in API publish route

--- app/api/publish/route.ts
+++ app/api/publish/route.ts
@@ -14,11 +14,11 @@ export async function POST(req: Request) {
 
     await page.goto("https://shop.skoleom.com/wp-admin/post-new.php?post_type=video");
 
-    await page.locator('id=title').fill(body.title ?? '');
-    await page.locator('id=acf-field_687936b2b0e88').fill(`https://www.youtube.com/watch?v=${body.videoId}`);
-    await page.locator('id=acf-field_687936e0b0e89').fill(body.videoId ?? '');
-    await page.locator('id=acf-field_68f92eb225504').fill(body.duration ?? '');
-    await page.locator('id=acf-field_68793708b0e8a').fill(body.miniature ?? '');
+    await page.locator('id=title').fill(body.title ?? ''); // Title 
+    await page.locator('id=acf-field_687936b2b0e88').fill(`https://www.youtube.com/watch?v=${body.videoId}`); // Lien youtube 
+    await page.locator('id=acf-field_687936e0b0e89').fill(body.videoId ?? ''); // Video ID
+    await page.locator('id=acf-field_68f92eb225504').fill(body.duration ?? ''); // Durée de la vidéo
+    await page.locator('id=acf-field_68793708b0e8a').fill(body.miniature ?? ''); // Miniature
 
     if (body.collection_sesport !== false) {
       await page.locator('id=in-popular-collection-1443').click();
@@ -35,7 +35,6 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ success: true });
   } catch (err: any) {
-    await context.close();
     return NextResponse.json({ error: err?.message || String(err) }, { status: 500 });
   }
 }
